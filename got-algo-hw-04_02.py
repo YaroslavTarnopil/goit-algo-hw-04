66c781dcd443339f1e0b0068e14c0ff05f3c9f32
YaroslavTarnopil <156083714+YaroslavTarnopil@users.noreply.github.com>
2024-05-10 13:47:42 +0300

    Update got-algo-hw-04_02.py

--- got-algo-hw-04_02.py
+++ got-algo-hw-04_02.py
@@ -1,22 +1,20 @@
-with open("get_cats_info.txt","w") as fh:
-    fh.write("60b90c1c13067a15887e1ae1,Tayson,3\n60b90c2413067a15887e1ae2,Vika,1\n60b90c2e13067a15887e1ae3,Barsik,2\n60b90c3b13067a15887e1ae4,Simon,12\n60b90c4613067a15887e1ae5,Tessi,5")
-       
 def get_cats_info(path):
     try:
-        with open("get_cats_info.txt","r") as fh:
+        with open(path, "r") as fh:  
             lines = fh.readlines()
-            lists = []
-            dictionary = {}
-        for line in lines:
-            line = line.split(",")
-            lists.append(line)
-            cats_info = {}
-            dictionary["id"] = line[0]
-            dictionary["name"] = line[1]
-            dictionary["age"] = int(line[2])  
-    except:
+            cats_info = []  
+            for line in lines:
+                line = line.strip().split(",")  
+                cat_info = {"id": line[0], "name": line[1], "age": int(line[2])}
+                cats_info.append(cat_info)  
+    except FileNotFoundError:
         print("The file does not exist")
-    return(lists)
+        return None 
+    return cats_info
 
-cats_info = get_cats_info("get_cats_info.txt")
-print(cats_info)
+
+relative_path = "get_cats_info.txt"
+
+cats_info = get_cats_info(relative_path)
+if cats_info:
+    print(cats_info)
